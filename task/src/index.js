fc77eefa0423f6bf2661188b9fdb13d5db8dd39d
TAOUFIQNADIFI <35980879+TAOUFIQNADIFI@users.noreply.github.com>
2021-05-23 17:11:08 -0400

    new change

--- task/src/index.js
+++ task/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-   <App/>,
+<h1> Welcome React</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
