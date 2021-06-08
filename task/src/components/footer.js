1f3ce545c6ba00c8496a6bd9a1bed7271bd35c3c
TAOUFIQNADIFI <35980879+TAOUFIQNADIFI@users.noreply.github.com>
2021-06-08 14:39:23 -0400

    new change

--- task/src/components/footer.js
+++ task/src/components/footer.js
@@ -1,21 +1,13 @@
 import React  from 'react';
 import ReactDOM from 'react-dom';
-import './footer.css';
 import PopUp from './PopUp.js'
+import './footer.css';
+
 
 
 class Footer extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-          showPopup: false
-        };
-      }
-      togglePopup() {
-        this.setState({
-          showPopup: !this.state.showPopup
-        });
-      }
+  
+      
 
 
   
@@ -28,14 +20,14 @@ class Footer extends React.Component {
 
         //<!-- Footer -->
         <div className="row">
-        <div className="footer" onClick={this.togglePop}>
+        <div className="footer" >
             <div className="foot" >
+            <button type="button" className="btn btn-primary">RULES</button>
 
-  <h4>RULES</h4> 
+  
   
   </div>
 </div>
-{this.state.seen ? <PopUp toggle={this.togglePop} /> : null}
 </div>
       
 );
